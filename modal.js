3839a7c61b08eb60e074315c3dd502966d9ab122
unknown <saurabhkhoshya@gmail.com>
2021-12-10 01:18:08 +0530

    actions add, animation

--- modal.js
+++ modal.js
@@ -2,6 +2,7 @@ class Modal extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
+    this.isOpen;
     this.shadowRoot.innerHTML = `
         <style>
             #backdrop {
@@ -9,24 +10,123 @@ class Modal extends HTMLElement {
                 top:0;
                 left: 0;
                 width:100%;
-                height: 100%;
+                height: 100vh;
                 background: rgba(0,0,0, 0.75);
+                opacity: 0;
+                pointer-events: none;
             }
+
+            :host([opened]) #backdrop, 
+            :host([opened]) #modal {
+                opacity:1;
+                pointer-events: all;
+            }
+
+            :host([opened]) #modal {
+                top: 15vh;
+            }
+
             #modal {
                 position: fixed;
-                top: 15vh;
+                top: 10vh;
                 left: 25%;
                 width: 50%;
-                height: 30rem;
                 z-index: 100;
                 background: white;
                 border-radius: 3px;
                 box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+                opacity: 0;
+                pointer-events: none;
+                transition: all 0.3s ease-out;
+            }
+
+            header {
+                padding: 1rem;
+                border-bottom: 1px solid #ccc;
+            }
+
+            ::sloted(h1) {
+                font-size: 1.25rem;
+            }
+
+            #main {
+                padding: 1rem;
+            }
+
+            #actions {
+                border-top: 1px solid #ccc;
+                padding: 1rem;
+                display: flex;
+                justify-content: flex-end;
+            }
+
+            #actions button {
+                margin: 0. 0.25rem
             }
         </style>
         <div id="backdrop"></div>
-        <div id="modal"></div>
+        <div id="modal">
+            <header>
+                <slot name="title"></slot>
+            </header>
+            <section id="main">
+                <slot></slot>
+            </section>
+            <section id="actions">
+                <button id="cancel-btn">Cancel</button>
+                <button id="confirm-btn">Confirm</button>
+            </section>
+        </div>
     `;
+    const slots = this.shadowRoot.querySelectorAll("slot");
+    slots[1].addEventListener("slotchange", (event) => {
+      console.dir(slots[1].assignedElements());
+    });
+    const backdrop = this.shadowRoot.querySelector("#backdrop");
+    const cancelButton = this.shadowRoot.querySelector("#cancel-btn");
+    const confirmButton = this.shadowRoot.querySelector("#confirm-btn");
+    cancelButton.addEventListener("click", this._cancel.bind(this));
+    confirmButton.addEventListener("click", this._confirm.bind(this));
+    cancelButton.addEventListener("cancel", () => {
+      console.log("Cancel this event");
+    });
+    backdrop.addEventListener("click", this._cancel.bind(this));
+  }
+
+  attribureChangedCallback(name, oldValue, newValue) {
+    if (name === "opened") {
+      if (this.hasAttribute("opened")) {
+      }
+    }
+  }
+
+  static get observedAttributes() {
+    return ["opened"];
+  }
+
+  open() {
+    this.setAttribute("opened", "");
+    this.isOpen = true;
+  }
+
+  hide() {
+    if (this.hasAttribute("opened")) this.removeAttribute("opened");
+    this.isOpen = false;
+  }
+
+  _cancel(event) {
+    this.hide();
+    const canceEvent = new Event("cancel", { bubbles: true, composed: true });
+    event.target.dispatchEvent(canceEvent);
+  }
+
+  _confirm() {
+    this.hide();
+    const confirmEvent = new Event("confirm");
+    this.dispatchEvent(confirmEvent);
   }
 }
 
